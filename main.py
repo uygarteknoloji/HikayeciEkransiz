a255d0663192468e8e074281c6e2d4f73f2c4736
uygarteknoloji <bilgisayarmuhendisi@hotmail.com>
2026-02-21 09:59:36 +0300

    bbb

--- main.py
+++ main.py
@@ -54,9 +54,9 @@ def print_image_to_printer(image_path):
     time.sleep(0.2)
 
 def text_to_image_80mm(file_path, output_path="print_temp.png"):
-    WIDTH = 532 # Daha önce belirlediğimiz çalışan genişlik
-    MARGIN_X = 20
-    MARGIN_Y = 20
+    WIDTH = 512 # Daha önce belirlediğimiz çalışan genişlik
+    MARGIN_X = 5
+    MARGIN_Y = 5
 
     with open(file_path, "r", encoding="utf-8") as f:
         lines = f.read().splitlines()
