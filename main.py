06fdd1927ee4b38adb8f7fd26758d802e123d952
uygarteknoloji <bilgisayarmuhendisi@hotmail.com>
2026-02-21 09:35:54 +0300

    Final

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from escpos.printer import Usb
 from PIL import Image, ImageDraw, ImageFont
 from threading import Thread
 
-TITLE_FONT_SIZE = 34
-BODY_FONT_SIZE = 24
+TITLE_FONT_SIZE = 24
+BODY_FONT_SIZE = 12
 LINE_SPACING = 10
 
 
@@ -89,7 +89,7 @@ def text_to_image_80mm(file_path, output_path="print_temp.png"):
         # KOYULAŞTIRMA AYARI: 
         # Değeri (80) ne kadar düşürürsen o kadar çok gri alan SİYAHA dönüşür.
         # Eğer hala çok açıksa 60 yapabilirsin.
-        logo_img = logo_img.point(lambda x: 0 if x < 60 else 255, '1') 
+        logo_img = logo_img.point(lambda x: 0 if x < 160 else 255, '1') 
         logo_img = logo_img.convert("L")
 
     total_height = (
